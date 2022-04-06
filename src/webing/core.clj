881a917571071eb6bddf280f092a8c67a03a8778
Malcolm Stone <malcolm.stone91@gmail.com>
2022-04-06 10:33:20 +0100

    rework path logic

--- src/webing/core.clj
+++ src/webing/core.clj
@@ -6,50 +6,42 @@
             [hiccup.core :as h]
             [mount.core :refer [defstate] :as mount]))
 
-(defstate test
-  :start 1)
-(mount/start)
-(mount/stop)
-test
-;; (def router
-;;   (r/router
-;;    [["/" ::home]
-;;     ["/api/ping" ::ping]
-;;     ["/api/orders/:id" ::order]]))
+;; (defstate test
+;;   :start 1)
+;; (mount/start)
+;; (mount/stop)
 
-;; (def router2
-;;   (ring/router
-;;    [["/test" ::test {:get (fn [] {:status 200 :body "Hello"})}]
-;;     ["/api" ::home]
-;;     ["/api/ping" ::ping]
-;;     ["/api/orders/:id" ::order]]))
-;; (r/match-by-name)
-;; (def app
-;;   (ring/ring-handler router2))
-
-;; (app {:request-method :get
-;;       :uri "/api/test"
-;;       :query-params {:x "1", :y "2"}})
 (def posts
-  [{:id 1 :slug "test" :title "Test"}])
+  [{:id 1 :slug "test" :title "Test"}
+   {:id 2 :slug "spring-tips" :title "Spring tips"}
+   {:id 3 :slug "my-clojure-journey" :title "My Clojure journey"}])
 
 (defn find-by-slug
   [slug]
   (->> posts (filter #(= slug (% :slug))) first))
 
+
+(defn path-to
+  ([router path-name]
+   (path-to router path-name {}))
+  ([router path-name path-params]
+   (-> router
+       (r/match-by-name path-name path-params)
+       (r/match->path))))
+
 (defn posts-view
-  []
+  [router]
   [:div
    [:h1 "Posts2"]
    [:ul
     (for [{slug :slug title :title} posts]
-      [:li [:a {:href (path-to ::posts-slug {:slug slug})} title]])]])
+      [:li [:a {:href (path-to router ::posts-slug {:slug slug})} title]])]])
 
 (defn post-view
-  [{title :title}]
+  [router {title :title}]
   [:div
    [:h1 title]
-   [:a {:href (path-to ::posts)} "Back to posts"]])
+   [:a {:href (path-to router ::posts)} "Back to posts"]])
 
 (def router
   (ring/router
@@ -57,25 +49,19 @@ test
           :get (fn [req] {:status 200 :body "Home"})}]
     ["/test" {:get (fn [req] {:status 200 :body "Hello there"})}]
     ["/echo" {:get (fn [req] {:status 200 :body (with-out-str (pprint/pprint req))})}]
-    ["/posts" {:name ::posts
-               :get (fn [req] {:status 200 :body
-                               (h/html (posts-view))})}]
+    ["/posts"
+     {:name ::posts
+      :get (fn [{router :reitit.core/router}]
+             {:status 200 :body
+              (h/html (posts-view router))})}]
     ["/posts/:slug"
      {:name ::posts-slug
-      :get (fn [{{slug :slug} :path-params}]
+      :get (fn [{{slug :slug} :path-params
+                 router :reitit.core/router}]
              (let [post (find-by-slug slug)]
-               {:status 200 :body (h/html (post-view post))}))}]]))
+               {:status 200 :body (h/html (post-view router post))}))}]]))
 (def app
   (ring/ring-handler router))
-
-(defn path-to
-  ([path-name]
-   (path-to path-name {}))
-  ([path-name path-params]
-   (-> app
-       (ring/get-router)
-       (r/match-by-name path-name path-params)
-       (r/match->path))))
 
 (def server
   (jetty/run-jetty
